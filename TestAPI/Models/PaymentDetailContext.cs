85bd1e23c4467348dddba41f493baca96712f967
sudheeshrama <sudheeshrama@gmail.com>
2020-06-07 16:16:39 +0530

    Add project files.

---
+++ TestAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAPI.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options)
+        {
+        }
+
+        public DbSet<PaymentDetails> PaymentDetails { get; set; }
+    }
+}
